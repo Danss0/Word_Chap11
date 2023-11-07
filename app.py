5cad431b7db3535793f46ea27abb43cb0f4fda1a
Danss0 <rizkydante0@gmail.com>
2023-11-07 11:53:00 +0700

    Mengubah Versi

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
 
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
